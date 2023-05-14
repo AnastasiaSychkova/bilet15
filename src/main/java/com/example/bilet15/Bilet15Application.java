4fd74c586a8d50e3db7e04a793e9d85d6214788c
Anastasia <sychkova.nastyusha@mail.ru>
2023-05-14 18:09:42 +0400

    Initial commit

---
+++ src/main/java/com/example/bilet15/Bilet15Application.java
@@ -0,0 +1,13 @@
+package com.example.bilet15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bilet15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bilet15Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilet15/Bilet15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bilet15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bilet15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
